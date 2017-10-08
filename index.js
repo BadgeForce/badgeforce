156e4b2171e43a99fb366a537196cea8c621a347
Khalil Claybon <khalil.claybon@yodle.com>
2017-10-08 14:52:30 -0400

    use require

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import issuer_artifacts from './build/contracts/Issuer.json';
-import holder_artifacts from './build/contracts/Holder.json';
+const issuer_artifacts = require('./build/contracts/Issuer.json');
+const holder_artifacts = require('./build/contracts/Holder.json');
 
 export default {
     issuer: issuer_artifacts, 
