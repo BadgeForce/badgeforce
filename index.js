fff167d568f0c5f7f3f76d43696b3dd7fb4f41b8
Khalil Claybon <khalil.claybon@yodle.com>
2018-01-05 04:02:08 -0500

    require

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const issuer_artifacts = artifacts.require('./build/contracts/Issuer.json');
 const holder_artifacts = artifacts.require('./build/contracts/Holder.json');
-
+const badgeLibrary_artifacts = artifacts.require('./build/contracts/BadgeLibrary.json');
 module.exports = {
     issuer: issuer_artifacts, 
-    holder: holder_artifacts
+    holder: holder_artifacts,
+    badgeLibrary: badgeLibrary_artifacts
 };
